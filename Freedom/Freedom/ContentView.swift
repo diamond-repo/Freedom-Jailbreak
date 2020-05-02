fcd2946bb7630eee4898724243d78621097663eb
ByteRhino <49131430+ModdedBuilder@users.noreply.github.com>
2020-05-02 08:47:05 -0400

    Add files via upload

---
+++ Freedom/Freedom/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Freedom
+//
+//  Created by Adam Smith on 5/2/20.
+//  Copyright © 2020 FreedomJB Team. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
